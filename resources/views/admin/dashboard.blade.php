be499ed85843f1cc66d6d30d666902d4b6bd04a8
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-05-24 12:36:29 +0100

    WIP

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,26 +8,15 @@
         <div class="row">
             <div class="col-md-3">
                 @role('admin')
-                <div class="menu-admin">
-                    <ul>
-                        <li><a href="#">Link 1</a></li>
-                        <li><a href="#">Link 2</a></li>
-                        <li><a href="#">Link 3</a></li>
-                    </ul>
-                </div>
-                @else
-                    <div class="menu-users">
-                        <ul>
-                            <li><a href="#">User Link 1</a></li>
-                            <li><a href="#">User Link 2</a></li>
-                            <li><a href="#">User Link 3</a></li>
-                        </ul>
-                    </div>
-                    @endrole
+                <!-- Admin session -->
+                @endrole
+                @role('client')
+                <!-- Client session -->
+                @endrole
             </div>
-            <div class="col-md-9">
-                <h1>Dashboard</h1>
-            </div>
+        </div>
+        <div class="col-md-9">
+            <h1>Dashboard</h1>
         </div>
     </div>
 @endsection
