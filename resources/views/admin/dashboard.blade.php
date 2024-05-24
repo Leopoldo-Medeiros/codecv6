1430f74996b3e41197bb25d865a97486d1c268b0
Leopoldo <32571375+Leopoldo-Medeiros@users.noreply.github.com>
2024-05-24 15:44:45 +0100

    WIP Menu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,15 +8,22 @@
         <div class="row">
             <div class="col-md-3">
                 @role('admin')
-                <!-- Admin session -->
+                <!-- Menu Admin -->
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="{{ route('admin.page') }}">Area Admin</a></li>
+                    <li class="list-group-item"><a href="{{ route('admin.logout') }}">Logout</a></li>
+                </ul>
                 @endrole
                 @role('client')
-                <!-- Client session -->
+                    <!-- Menu Client -->
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="{{ route('client.page') }}">Area Client</a></li>
+                    </ul>
                 @endrole
             </div>
-        </div>
-        <div class="col-md-9">
-            <h1>Dashboard</h1>
+            <div class="col-md-9">
+                <h1>Dashboard</h1>
+            </div>
         </div>
     </div>
 @endsection
