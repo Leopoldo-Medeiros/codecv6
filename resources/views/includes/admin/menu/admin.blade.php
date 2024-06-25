46da45234c066d6fe85ce8e4b2a3377864cfabc7
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2024-06-25 01:25:55 +0100

    :construction: WIP: Create(CRUD)

--- resources/views/includes/admin/menu/admin.blade.php
+++ resources/views/includes/admin/menu/admin.blade.php
@@ -1,32 +1,22 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 <ul class="nav nav-pills flex-column mb-auto">
-    <li class="nav-item">
-        <a href="{{ route('users.index') }}" class="nav-link text-white">
-            <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-            Clients
+    <li class="nav-item dropdown">
+        <a class="btn btn--blue-400 dropdown-toggle px-4" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            MENU
         </a>
-    </li>
-    <li>
-        <a href="#" class="nav-link text-white">
-            <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-            Courses
-        </a>
-    </li>
-    <li>
-        <a href="#" class="nav-link text-white">
-            <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
-            Paths
-        </a>
-    </li>
-    <li>
-        <a href="#" class="nav-link text-white">
-            <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
-            Steps
-        </a>
-    </li>
-    <li>
-        <a href="{{ route('logout') }}" class="nav-link text-white">
-            <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-            Logout
-        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item hover--blue-200" href="{{ route('users.index') }}">Clients</a></li>
+            <li><a class="dropdown-item hover--blue-200" href="#">Courses</a></li>
+            <li><a class="dropdown-item hover--blue-200" href="#">Paths</a></li>
+            <li><a class="dropdown-item hover--blue-200" href="#">Steps</a></li>
+            <li><a class="dropdown-item hover--blue-200" href="{{ route('logout') }}">Logout</a></li>
+        </ul>
     </li>
 </ul>
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Edit user</h1>
+    <form action="{{ route('users.update') }}" method="PUT">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" value="{{ $user->name }}" required>
+        </div>
+        <div class="form-group">
+            <label for="name">Email</label>
+            <input type="email" class="form-control" id="email" value="{{ $user->email }}" required>
+        </div>
+        <div class="form-group">
+            <label for="name">Password</label>
+            <input type="password" class="form-control" id="password" value="{{ $user->password }}" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+@endsection
