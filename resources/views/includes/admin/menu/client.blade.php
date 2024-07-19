8d912293b692224f2a142d4f3508c684ff6c65ca
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2024-07-19 12:44:46 +0100

    :bug: WIP: Menu Client

--- resources/views/includes/admin/menu/client.blade.php
+++ resources/views/includes/admin/menu/client.blade.php
@@ -5,22 +5,22 @@
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
             <a href="{{ route('users.index') }}" class="nav-link bg-blue-400 text-black py-2 px-4 rounded hover:bg-blue-500 hover:text-white flex items-center">
-                <i class="fas fa-users mt-4 mr-2"></i> Clients
+                <i class="fas fa-graduation-cap mt-4 mr-2"></i> My Courses
             </a>
         </li>
         <li>
             <a href="#" class="nav-link bg-blue-400 text-black py-2 px-4 rounded hover:bg-blue-500 hover:text-white flex items-center">
-                <i class="fas fa-book mr-2"></i> Courses
+                <i class="fas fa-route mr-2"></i> My Paths
             </a>
         </li>
         <li>
             <a href="#" class="nav-link bg-blue-400 text-black py-2 px-4 rounded hover:bg-blue-500 hover:text-white flex items-center">
-                <i class="fas fa-map-signs mr-2"></i> Paths
+                <i class="fas fa-id-card mr-2"></i> My CV
             </a>
         </li>
         <li>
             <a href="#" class="nav-link bg-blue-400 text-black py-2 px-4 rounded hover:bg-blue-500 hover:text-white flex items-center">
-                <i class="fas fa-tasks mr-2"></i> Steps
+                <i class="fas fa-folder-open mr-2"></i> My Files
             </a>
         </li>
         <li>
