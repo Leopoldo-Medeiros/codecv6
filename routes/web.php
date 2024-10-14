93996799b418075773f5c5646ffe7e7687258ab1
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2024-10-14 15:10:07 +0100

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,21 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pricing', function () {
+    return view('welcome');
+})->name('pricing');
+Route::get('/about-us', function () {
+    return view('welcome');
+})->name('about-us');
+
 // Public routes
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Rotas protegidas (usuários autenticados)
